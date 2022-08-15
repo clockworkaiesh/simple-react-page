092c01cf94d457bcec85be3b54186cc50c590243
ayesha <ayesha@nxtdevs.com>
2022-08-15 16:25:58 +0500

    First commit

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Footer(){
+    return(
+      <div className="footer">
+        <p>All rights reserver copyrights &copy;{new Date().getFullYear()} . Developed by Ayesha Naveed</p>
+      </div>
+    )
+  }
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Header(){
+    return(
+      <div className="header">
+        <nav>
+          <img src="./react-logo.png" alt="logo" className='logo'/>
+          <p>React Course - Project 1</p>
+        </nav>
+      </div>
+    )
+  }
